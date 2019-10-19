81cc9835935f4ab60773e1287b0728e393662fce
leapwill <leapwill@users.noreply.github.com>
2019-10-19 12:50:26 -0500

    Animate hamburger menu to X instead of using icon
    On hold because of jank in Chromium. Specifically, the rotation is performed before the transform-origin is applied.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,18 @@ function toggleTheme() {
 
 function toggleMenu(forcedState) {
   let el = document.querySelector('nav');
+  let el2 = document.querySelector('#btn-menu > div');
   if (typeof forcedState === 'undefined') {
     el.classList.toggle('nav-open');
+    el2.classList.toggle('nav-open');
   }
   else if (forcedState === false) {
     el.classList.remove('nav-open');
+    el2.classList.remove('nav-open');
   }
   else if (forcedState === true) {
     el.classList.add('nav-open');
+    el2.classList.add('nav-open');
   }
 }
 
