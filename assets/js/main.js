d394eca6d5e2d78c5f230a5ab82b1a150152d57c
leapwill <leapwill@users.noreply.github.com>
2019-11-23 09:56:44 -0600

    Animate hamburger button

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,18 @@
-
 function toggleTheme() {
-  document.querySelector('body').classList.toggle('theme-dark');
+  document.querySelector("body").classList.toggle("theme-dark");
 }
 
 function toggleMenu(forcedState) {
-  let el = document.querySelector('nav');
-  if (typeof forcedState === 'undefined') {
-    el.classList.toggle('nav-open');
-  }
-  else if (forcedState === false) {
-    el.classList.remove('nav-open');
-  }
-  else if (forcedState === true) {
-    el.classList.add('nav-open');
+  let el = document.querySelector("nav");
+  let el2 = document.querySelector("#btn-menu > div");
+  if (typeof forcedState === "undefined") {
+    el.classList.toggle("nav-open");
+    el2.classList.toggle("nav-open");
+  } else if (forcedState === false) {
+    el.classList.remove("nav-open");
+    el2.classList.remove("nav-open");
+  } else if (forcedState === true) {
+    el.classList.add("nav-open");
+    el2.classList.add("nav-open");
   }
 }
-
-
